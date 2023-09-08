3515e152b92ddbd70d00ea919020a532c215c6a6
Tharith Yeak <tyea0002@student.monash.edu>
2023-09-08 16:04:16 +1000

    fix path names for home and project_backlog in urls.py

--- project_management_app/project_task/urls.py
+++ project_management_app/project_task/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_redirect, name="Home"),
-    path("home/", views.home, name="Home"),
-    path("project-backlog/", views.project_backlog, name="Project Backlog"),
+    path("", views.home_redirect, name="home"),
+    path("home/", views.home, name="home"),
+    path("project-backlog/", views.project_backlog, name="project_backlog"),
 ]
