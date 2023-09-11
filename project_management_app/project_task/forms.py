82190e86798d2ba247d657e9e39df54f80ee28a5
Tharith Yeak <tyea0002@student.monash.edu>
2023-09-12 00:47:03 +1000

    Create new form for entering New Task

---
+++ project_management_app/project_task/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Task, Tag
+
+
+class CreateNewTaskForm(forms.ModelForm):
+    """
+    A form for creating a new task.
+    """
+    # For the tags, use Django's built-in SelectMultiple widget
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
+        required=False
+    )
+
+    class Meta:
+        model = Task
+        fields = [
+            "name",
+            "type",
+            "priority",
+            "description",
+            "story_point",
+            "assignee",
+            "tags",
+            "status",
+            "stage",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(CreateNewTaskForm, self).__init__(*args, **kwargs)
