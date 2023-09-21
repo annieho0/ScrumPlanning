0208104b83f39fbc8769fde78c8ebd2b8702ec1b
Tharith Yeak <tyea0002@student.monash.edu>
2023-09-22 01:06:57 +1000

    Fix url to match views

--- project_management_app/project_task/urls.py
+++ project_management_app/project_task/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import HomeView, ProjectBacklogView, TaskDeleteView
+from .views import HomeListView, TaskListView, TaskEditView, TaskDeleteView
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home_redirect"),
-    path("home/", HomeView.as_view(), name="home"),
-    path("project-backlog/", ProjectBacklogView.as_view(), name="project_backlog"),
-    path('project-backlog/delete_task/<int:pk>/', TaskDeleteView.as_view(), name='delete_task'),
+    path("", HomeListView.as_view(), name="home_redirect"),
+    path("home/", HomeListView.as_view(), name="home"),
+    path("project-backlog/", TaskListView.as_view(), name="project_backlog"),
+    path("project-backlog/edit_task/<int:task_id>/", TaskEditView.as_view(), name="edit_task"),
+    path("project-backlog/delete_task/<int:task_id>/", TaskDeleteView.as_view(), name="delete_task"),
 ]
+
