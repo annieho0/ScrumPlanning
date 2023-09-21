f70aceb0853fdbbf3e7b9af953feb5fa03967020
Tharith Yeak <tyea0002@student.monash.edu>
2023-09-21 22:18:06 +1000

    Improved the view codes

--- project_management_app/project_task/views.py
+++ project_management_app/project_task/views.py
@@ -3,9 +3,9 @@ from django.contrib import messages
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views import View
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from .models import Tag, Task
-from .forms import CreateNewTaskForm
+from .forms import CreateNewTaskForm, EditTaskForm
 from django.db.models import Case, When, Value, IntegerField
 import logging
 
@@ -19,24 +19,53 @@ class TaskManager:
     """
 
     @staticmethod
-    def create_task(cleaned_data):
+    def create_task(data):
         """
         This method creates a new task.
+        It will validate the task and return a success or error message.
+        It will also create the tags if they don't exist.
         """
 
-        tags = cleaned_data.get('tags', [])  # Directly use 'get' for dictionaries
-        tag_objects = [Tag.objects.get_or_create(name=tag_name)[0] for tag_name in tags]
+        # Create a new task based on the form data
+        task_form = CreateNewTaskForm(data)
 
-        task = CreateNewTaskForm(cleaned_data).save(commit=False)
-        task.save()
-        for tag in tag_objects:
-            task.tags.add(tag)
-        return True, f"Task '{str(task)}' successfully created!"
+        # validate the form
+        if task_form.is_valid():
+            # Create the task
+            cleaned_data = task_form.cleaned_data
+
+            # Get the tags from the form data and create them if they don't exist
+            tags = cleaned_data.get('tags', [])  # Directly use 'get' for dictionaries
+            tag_objects = [Tag.objects.get_or_create(name=tag_name)[0] for tag_name in tags]
+
+            # Create the task but don't save it yet
+            task = CreateNewTaskForm(cleaned_data).save(commit=False)
+
+            # Add the tags to the task
+            for tag in tag_objects:
+                task.tags.add(tag)
+
+            # Save the task and return success message
+            task.save()
+            return True, f"Task '{str(task)}' successfully created!"
+
+        else:
+            # error can be a dictionary if more than one error or a string if only one error
+            # If the error message is a dictionary, convert it to a string
+            if isinstance(task_form.errors, dict):  # If errors are returned as a dict
+                error_message = '; '.join([': '.join([key, val[0]]) for key, val in task_form.errors.items()])
+            else:
+                error_message = task_form.errors
+
+            # Return error message
+            return False, error_message
 
     @staticmethod
     def read_task(task_id):
         """
         This method reads a task.
+        If the task does not exist, it will return None instead of raising an error.
+        If the task exists, it will return the task object.
         """
         try:
             return Task.objects.get(id=task_id)
@@ -48,12 +77,32 @@ class TaskManager:
         """
         This method updates a task.
         """
+
+        # Check if the task exists
         task = TaskManager.read_task(task_id)
         if not task:
-            return False, "Task not found"
+            return False, "The Task does not exist"
 
-        form = CreateNewTaskForm(data, instance=task)
-        if form.is_valid():
+        # Validate the task data is valid
+        update_form = EditTaskForm(data, instance=task)
+
+        if update_form.is_valid():
+
+            # get the cleaned data
+            cleaned_data = update_form.cleaned_data
+
+            # Get the tags from the form data and create them if they don't exist
+            tags = cleaned_data.get('tags', [])  # Directly use 'get' for dictionaries
+            tag_objects = [Tag.objects.get_or_create(name=tag_name)[0] for tag_name in tags]
+
+            # Add the tags to the task
+            for tag in tag_objects:
+                task.tags.add(tag)
+
+            # Save the task and return success message
+            task.save()
+            return True, f"Task '{str(task)}' successfully created!"
+
             form.save()
             return True, f"Task '{str(task)}' successfully updated!"
         else:
@@ -64,29 +113,41 @@ class TaskManager:
         """
         This method deletes a task.
         """
+
+        # Check if the task exists
         task = TaskManager.read_task(task_id)
         if not task:
-            return False, "Task not found"
+            return False, "Task not found for deletion"
 
+        # Get the task name for the feedback message
+        task_name = task.name
+
+        # Delete the task
         task.delete()
-        return True, "Task successfully deleted!"
+
+        return True, f"Task '{task_name}'successfully deleted!"
 
     @staticmethod
-    def list_tasks(filter_criteria=None, priority_sort_value=None, tag_filter_value=[]):
+    def list_tasks(tag_filter_criteria=None, priority_sort_value=None, tag_filter_value=None):
         """
         List tasks based on some filtering criteria and sorting parameters.
-        :param filter_criteria: A dictionary with model fields as keys.
+        :param tag_filter_criteria: A dictionary with model fields as keys.
         :param priority_sort_value: A string representing the model field to sort by.
         :param tag_filter_value: A list of tag names to filter by.
         :return: A queryset of matching tasks.
         """
-        tasks = Task.objects.filter(**filter_criteria) if filter_criteria else Task.objects.all()
 
-        # Filter by tags
+        # Set tag filter criteria
+        if tag_filter_value is None:
+            tag_filter_value = []
+        tasks = Task.objects.filter(**tag_filter_criteria) if tag_filter_criteria else Task.objects.all()
+
+        # Filter by user selected tags
         if tag_filter_value:
             tasks = tasks.filter(tags__name__in=tag_filter_value).distinct()
 
-        # Sort by priority
+        # Sort by user selected priority
+        # sort by priority ascending
         if priority_sort_value == "priority_ascending":
             tasks = tasks.annotate(
                 custom_priority_order=Case(
@@ -98,6 +159,7 @@ class TaskManager:
                     output_field=IntegerField()
                 )
             ).order_by("custom_priority_order")
+        # sort by priority descending
         elif priority_sort_value == "priority_descending":
             tasks = tasks.annotate(
                 custom_priority_order=Case(
@@ -111,6 +173,7 @@ class TaskManager:
             ).order_by("custom_priority_order")
         elif priority_sort_value:
             tasks = tasks.order_by(priority_sort_value)
+
         return tasks
 
 
@@ -119,8 +182,7 @@ class HomeView(View):
     This class-based view renders the home page.
     """
 
-    @staticmethod
-    def get(request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         """
         This method renders the home page.
         """
@@ -134,21 +196,28 @@ class ProjectBacklogView(View):
     template_name = "project_task/project_backlog.html"
 
     def get(self, request, *args, **kwargs):
+
+        # Generate an empty form
         form = CreateNewTaskForm()
 
+        # Get the sorting and view parameters from the URL, if empty, use default values
         priority_sort = request.GET.get('priority_sort', 'priority_ascending')
         current_view = request.GET.get('view', 'list_view')
 
         # Filter Tags that is in use currently from the database
         used_tags_ids = Task.objects.values_list('tags', flat=True).distinct()
         tags = Tag.objects.filter(id__in=used_tags_ids)
+
         # Clean up the selected tags passed by the URL
         selected_tags_string = request.GET.get('tags_filter', '')
         selected_tags = selected_tags_string.split(",") if selected_tags_string else []
 
-        print(selected_tags)
-        tasks = TaskManager.list_tasks(filter_criteria={"sprint": None}, priority_sort_value=priority_sort,
+        # Get the tasks based on the filtering and sorting parameters
+        tasks = TaskManager.list_tasks(tag_filter_criteria={"sprint": None},
+                                       priority_sort_value=priority_sort,
                                        tag_filter_value=selected_tags)
+
+        # Render the template with the tasks and tags
         return render(request, self.template_name, {
             "name": "project-backlog",
             "tasks": tasks,
@@ -163,22 +232,28 @@ class ProjectBacklogView(View):
         """
         This method handles the task creation.
         """
+
+        # Create a new task based on the form data
         form = CreateNewTaskForm(self.request.POST)
+
+        # Check if the form is valid
         if form.is_valid():
+            # Create the task
             success, message = TaskManager.create_task(form.cleaned_data)
-            if success:
-                tags_list = [str(tag) for tag in form.cleaned_data['tags']]
-                return JsonResponse(
-                    {'status': 'success', 'message': message, 'task': {**form.cleaned_data, 'tags': tags_list}})
-            else:
-                if isinstance(message, dict):  # If errors are returned as a dict
-                    error_message = '; '.join([': '.join([key, val[0]]) for key, val in message.items()])
-                else:
-                    error_message = message
-                return JsonResponse({'status': 'error', 'message': error_message})
+            # Send success feedback to the user
+            tags_list = [str(tag) for tag in form.cleaned_data['tags']]
+
+            return JsonResponse({'status': 'success', 'message': message,
+                                 'task': {**form.cleaned_data, 'tags': tags_list}})
+
+        # Send error feedback to the user
         else:
-            error_message = 'There were errors in your submission. Please correct them and try again.'
-            logger.error(f"Form submission errors: {form.errors}")  # Log the errors for debugging
+            # If the error message is a dictionary, convert it to a string
+            if isinstance(form.errors, dict):  # If errors are returned as a dict
+                error_message = '; '.join([': '.join([key, val[0]]) for key, val in form.errors.items()])
+            else:
+                error_message = form.errors
+
             return JsonResponse({'status': 'error', 'message': error_message})
 
 
@@ -188,7 +263,7 @@ class TaskDeleteView(DeleteView):
     """
     model = Task
 
-    def get(self, request, *args, **kwargs):
+    def get(self, *args, **kwargs):
         # TODO: Uncomment this section to enable backend check authenticated user for deletion
         """
         # A simple backend check to ensure user has the right to delete. Adjust as needed.
@@ -198,24 +273,55 @@ class TaskDeleteView(DeleteView):
                 'message': "You don't have permission to delete this task."
             })
         """
-        return self.delete(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
-        """
-        This method deletes a task.
-        """
-        try:
-            self.object = self.get_object()
-            task_name = self.object.name  # Capture the task name for the feedback message
-            self.object.delete()
+        # Get the task id from the URL
+        task_id = self.kwargs.get('pk')  # 'pk' is the default name for the primary key field
+        # delete the task can collect feedback
+        status, message = TaskManager.delete_task(task_id)
+        if status:
             # Send success feedback
-            return JsonResponse({
-                'status': 'success',
-                'message': f"Task '{task_name}' was successfully deleted!"
-            })
-        except Exception as e:
-            # Handle any error during deletion and send error feedback
-            return JsonResponse({
-                'status': 'error',
-                'message': f"Error deleting task: {str(e)}"
-            })
+            return JsonResponse({'status': 'success', 'message': message})
+        else:
+            # Send error feedback
+            return JsonResponse({'status': 'error', 'message': message})
+
+
+class EditTaskView(UpdateView):
+    """
+    This class-based view edits a task.
+    """
+
+    def get(self, *args, **kwargs):
+        """
+        This method renders the edit task page.
+        """
+        # Get the task id from the URL
+        task_id = self.kwargs.get('pk')  # 'pk' is the default name for the primary key field
+
+        # Get the task
+        task = TaskManager.read_task(task_id)
+
+        # Check if the task exists
+        if not task:
+            messages.error(self.request, "Task does not exist")
+            return redirect("project_task:project-backlog")
+
+        # Generate a form with the task data
+        edit_form = EditTaskForm(instance=task)
+
+        # Render the template with the form
+        return render(self.request, "project_task/edit_task.html", {
+            "name": "edit-task",
+            "edit_form": edit_form,
+            "task": task
+        })
+
+    def post(self, *args, **kwargs):
+        """
+        This method handle task edit.
+        """
+
+        # Get the task id from the URL
+        task_id = self.kwargs.get('pk')  # 'pk' is the default name for the primary key field
+
+        # edit
