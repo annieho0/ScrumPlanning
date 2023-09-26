71e8b509f4c9f5786d7c09ed1aff177dd626a818
Tharith Yeak <tyea0002@student.monash.edu>
2023-09-26 18:20:16 +1000

    update login page for with styling including the errors message

--- project_management_app/register/views.py
+++ project_management_app/register/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import RegisterFrom
 from django.db import IntegrityError
 from django.contrib.auth import login
-from .authentication import UserAuthentication
+from django.contrib.auth.forms import AuthenticationForm
 
 def admin_required(func):
     def _wrapped_view(request, *args, **kwargs):
@@ -30,3 +30,26 @@ def register(response):
         form = RegisterFrom()
     print("Hello")
     return render(response, "register/register.html", {"form": form})
+
+
+def login(request):
+    form_submitted = False
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        form_submitted = True
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('some-success-url')  # Redirect to wherever you want after a successful login
+
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        'form': form,
+        'form_submitted': form_submitted
+    }
+
+    return render(request, 'path_to_your_template.html', context)
