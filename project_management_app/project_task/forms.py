63b084675a8242ae89a6e23f25301bffff66a3b4
Annie <ahoo0034@student.monash.edu>
2023-10-16 13:17:29 +1100

    Fixed the modal for the tasks modal is populated added the compatibility section for the card-view added the sprint name to the sprint board

--- project_management_app/project_task/forms.py
+++ project_management_app/project_task/forms.py
@@ -7,6 +7,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, HTML
 
 
+
 class CreateNewTaskForm(forms.ModelForm):
     """
     A form for creating a new task.
@@ -88,48 +89,49 @@ class EditTaskForm(forms.ModelForm):
         super(EditTaskForm, self).__init__(*args, **kwargs)
         self.fields['created_date'].initial = timezone.now()
 
-class ReadOnlyTextInput(forms.TextInput):
-    def render(self, name, value, attrs=None, renderer=None):
-        if self.attrs.get('readonly', False):
-            attrs = dict(attrs, readonly='readonly')
-        return super().render(name, value, attrs, renderer)
+class CreateNewSprintForm(forms.ModelForm):
+    class Meta:
+        model = Sprint
+        fields = [
+            "name",
+            "start_date",
+            "end_date",
+        ]
+
+    widgets = {
+    'start_date': forms.DateInput(),
+    'end_date': forms.DateInput()
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(CreateNewSprintForm, self).__init__(*args, **kwargs)
 
 
 class SprintBoardTaskForm(forms.ModelForm):
+
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
+        required=True
+    )
+
     class Meta:
         model = Task
-        fields = ('name', 'type', 'priority', 'description', 'story_point', 'tags', 'stage', 'created_date', 'status', 'assignee')
+        fields = [
+            "name",
+            "type",
+            "priority",
+            "description",
+            "story_point",
+            "tags",
+            "status",
+            "stage",
+            "assignee",
+            "created_date",
+        ]
 
     def __init__(self, *args, **kwargs):
         super(SprintBoardTaskForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'post'
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-lg-3'
-        self.helper.field_class = 'col-lg-9'
-        self.helper.layout = Layout(
-            'name',
-            'type',
-            'priority',
-            'description',
-            'story_point',
-            'tags',
-            'stage',
-            'created_date',
-            HTML('<hr>'),
-            'status',
-            'assignee',
-            HTML('<hr>'),
-            Submit('submit', 'Save')
-        )
-
-        # Make certain fields readonly
-        self.fields['name'].widget.attrs['readonly'] = True
-        self.fields['type'].widget.attrs['readonly'] = True
-        self.fields['priority'].widget.attrs['readonly'] = True
-        self.fields['description'].widget.attrs['readonly'] = True
-        self.fields['story_point'].widget.attrs['readonly'] = True
-        self.fields['tags'].widget.attrs['readonly'] = True
-        self.fields['stage'].widget.attrs['readonly'] = True
-        self.fields['created_date'].widget.attrs['readonly'] = True
-
+        for field_name in self.fields:
+            if field_name not in ["assignee", "status"]:
+                self.fields[field_name].disabled = True
