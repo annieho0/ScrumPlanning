433d9fa8612af1a6c4a188c639adb0a99c56fed5
NguyenJoe0201 <nguyenjoe0201@gmail.com>
2023-10-18 01:31:04 +1100

    revert the merge action

--- project_management_app/project_task/urls.py
+++ project_management_app/project_task/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path("project-backlog/", TaskListView.as_view(), name="project_backlog"),
     path("project-backlog/edit_task/<int:task_id>/", TaskEditView.as_view(), name="edit_task"),
     path("project-backlog/delete_task/<int:task_id>/", TaskDeleteView.as_view(), name="delete_task"),
-    # path("sprint-boards/get_updated_data/", SprintBoard.get_updated_data, name="get_updated_data"),
-    path("sprint-boards/get_task_data/<int:task_id>/", SprintBoard.get_task_data, name="get_task_data"),
-    path("sprint-boards/edit_tasks/<int:task_id>/", SprintBoard.edit_tasks, name="edit_tasks"),
+    # path("sprint-board/", SprintBoard.sprint_board, name="sprint_board"),
+    path("sprint-boards/update_task/<int:task_id>/", SprintBoard.update_task, name="update_task"),
+    path("sprint-boards/get_task/<int:task_id>/", SprintBoard.get_task, name="get_task"),
     path('sprint_boards/<int:sprint_id>/', SprintBoard.sprint_boards, name='sprint_boards'),
     # path('redirect_to_sprint_board/<int:sprint_id>/', SprintBoard.redirect_to_sprint_board, name='redirect_to_sprint_board'),
     
@@ -19,7 +19,5 @@ urlpatterns = [
     path('sprint_backlog/archive_sprint_backlog/<int:sprint_id>/', SprintBoard.archive_sprint_backlog, name='archive_sprint_backlog'),
 
     path("create-graph/", CreateGraphView.as_view(), name="create_graph"),
-    path('move_selected_tasks_to_sprint/', SprintBoard.move_selected_tasks, name='move_selected_tasks_to_sprint'),
-    #path("get_sprint_tasks/", SprintBoard.get_sprint_tasks, name="get_task")
 ]
 
